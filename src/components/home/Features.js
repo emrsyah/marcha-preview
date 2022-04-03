78b3aabbd73abcae3b6270405ad5a4fc545f6266
Emirsyah <muhammademir48@gmail.com>
2022-04-03 22:24:56 +0700

    added features component

--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -5,25 +5,25 @@ function Features() {
   return (
     <div className="mx-5 flex justify-between my-6">
       <div className="flex flex-col items-center gap-1">
-        <div className="text-blue-600 bg-blue-100 flex justify-center items-center p-3 max-w-max rounded-lg">
+        <div className="text-blue-600 bg-blue-100 hover:bg-blue-200 cursor-pointer flex justify-center items-center p-3 max-w-max rounded-lg ">
           <Icon icon="carbon:branch" width="30" />
         </div>
         <p className="text-xs font-medium">Split Bill</p>
       </div>
       <div className="flex flex-col items-center gap-1">
-        <div className="text-purple-600 bg-purple-100 flex justify-center items-center p-3 max-w-max rounded-lg">
+        <div className="text-purple-600 bg-purple-100 hover:bg-purple-200  cursor-pointer flex justify-center items-center p-3 max-w-max rounded-lg">
           <Icon icon="ph:users-three" width="30" />
         </div>
         <p className="text-xs font-medium">Group</p>
       </div>
       <div className="flex flex-col items-center gap-1">
-        <div className="text-yellow-600 bg-yellow-100 flex justify-center items-center p-3 max-w-max rounded-lg">
+        <div className="text-yellow-600 bg-yellow-100 hover:bg-yellow-200  cursor-pointer flex justify-center items-center p-3 max-w-max rounded-lg">
           <Icon icon="uil:credit-card" width="30" />
         </div>
         <p className="text-xs font-medium">Virtucard</p>
       </div>
       <div className="flex flex-col items-center gap-1">
-        <div className="text-green-600 bg-green-100 flex justify-center items-center p-3 max-w-max rounded-lg">
+        <div className="text-green-600 bg-green-100 flex hover:bg-green-200   cursor-pointer justify-center items-center p-3 max-w-max rounded-lg">
         <Icon icon="ph:circles-four-bold" width="30" />
         </div>
         <p className="text-xs font-medium">More</p>
