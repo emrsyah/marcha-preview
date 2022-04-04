09271aeda32d67f9e05f6a5f40759f09c85abb57
Emirsyah <muhammademir48@gmail.com>
2022-04-04 13:17:10 +0700

    payment out added

--- src/components/payment/Content.js
+++ src/components/payment/Content.js
@@ -8,7 +8,7 @@ const dataIn = [
   {
     id: 1,
     nama: 'Samuel',
-    tipe: 'Request',
+    tipe: 'in',
     jumlah: '18.000',
     note: 'Makan Padang',
     deadline: '17/04',
@@ -18,7 +18,30 @@ const dataIn = [
   {
     id: 2,
     nama: 'John',
-    tipe: 'Request',
+    tipe: 'in',
+    jumlah: '20.000',
+    note: 'Futsal',
+    deadline: '01/04',
+    status: 'Over Deadline',
+    gambar: foto2,
+  },
+]
+
+const dataOut = [
+  {
+    id: 1,
+    nama: 'Samuel',
+    tipe: 'out',
+    jumlah: '18.000',
+    note: 'Makan Padang',
+    deadline: '17/04',
+    status: 'Pending',
+    gambar: foto1,
+  },
+  {
+    id: 2,
+    nama: 'John',
+    tipe: 'out',
     jumlah: '20.000',
     note: 'Futsal',
     deadline: '01/04',
@@ -66,7 +89,13 @@ function Content() {
           ))}
           </div>
         </Tab.Panel>
-        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>
+        <div className="my-10 flex flex-col gap-7">
+          {dataOut.map(d=>(
+            <PaymentInCard key={d.id} data={d} />
+          ))}
+          </div>
+        </Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
     </div>
--- src/components/payment/PaymentInCard.js
+++ src/components/payment/PaymentInCard.js
@@ -1,30 +1,46 @@
-import React from 'react'
+import React from "react";
 
-
-function PaymentInCard({data}) {
+function PaymentInCard({ data }) {
   return (
-    <div className='py-3 px-5 bg-white' style={{boxShadow: '2px 2px 5px 0px #9D20FF1A'}} > 
-        <div className='grid grid-cols-8 gap-2'>
-            <img src={data.gambar} alt="" className='col-span-1 rounded-lg h-10 w-10' />
-            <div className='col-span-4 py-[2px]'>
-                <h5 className='text-sm font-medium'>{data.nama} - {data.tipe}</h5>
-                <h4 className='text-lg font-semibold'>Rp {data.jumlah}</h4>
-                <p className='text-sm'>Note: {data.note}</p>
-            </div>
-            <div className='col-span-3  flex flex-col items-end'>
-                <p className='text-sm text-left'>Due: {data.deadline}</p>
-                <p className={`text-sm font-medium ${data.status === "Pending" ? "text-[#ffa800]" : "text-red-600"}`}>{data.status}</p>
-            </div>
+    <div
+      className="py-3 px-5 bg-white"
+      style={{ boxShadow: "2px 2px 5px 0px #9D20FF1A" }}
+    >
+      <div className="grid grid-cols-8 gap-2">
+        <img
+          src={data.gambar}
+          alt=""
+          className="col-span-1 rounded-lg h-10 w-10"
+        />
+        <div className="col-span-4 py-[2px]">
+          <h5 className="text-sm font-medium">{data.tipe === "in" ? `${data.nama} - Request` : `Request to ${data.nama}` }</h5>
+          <h4 className="text-lg font-semibold">Rp {data.jumlah}</h4>
+          <p className="text-sm">Note: {data.note}</p>
         </div>
+        <div className="col-span-3  flex flex-col items-end">
+          <p className="text-sm text-left">Due: {data.deadline}</p>
+          <p
+            className={`text-sm font-medium ${
+              data.status === "Pending" ? "text-[#ffa800]" : "text-red-600"
+            }`}
+          >
+            {data.status}
+          </p>
+        </div>
+      </div>
 
-        {/* Button */}
-        <div className='grid grid-cols-9 gap-2 mt-4'>
-            <div className='col-span-1'></div>
-            <div className='font-medium py-2 px-10 bg-red-600 hover:bg-red-700 cursor-pointer text-white col-span-4 ml-1 rounded-md flex justify-center items-center'>Reject</div>
-            <div className='font-medium py-2 px-10 bg-green-600 hover:bg-green-700 cursor-pointer text-white col-span-4 ml-1 rounded-md flex justify-center items-center'>Accept</div>
+      {/* Button */}
+        <div className="grid grid-cols-9 gap-2 mt-4">
+          <div className="col-span-1"></div>
+          <div className={`font-medium py-2 px-10 cursor-pointer col-span-4 ml-1 rounded-md flex justify-center items-center  ${data.tipe === "in" ? "bg-red-600 hover:bg-red-700 text-white" : "border-purple-600 border-2 bg-white text-purple-600 hover:text-purple-700 hover:border-purple-700"}`}>
+            {data.tipe === "in"? "Reject" : "Cancel"}
+          </div>
+          <div className={`font-medium py-2 px-10 cursor-pointer col-span-4 ml-1 rounded-md flex justify-center items-center  ${data.tipe === "in" ? "bg-green-600 hover:bg-green-700 text-white" : "bg-purple-600 text-white hover:bg-purple-700"}`}>
+            {data.tipe === "in"? "Confirm" : "Remind"}
+          </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default PaymentInCard
+export default PaymentInCard;
